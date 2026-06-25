b525383d2dd45823d840d9424fd8256171fd3123
astral-0619 <3794477609@qq.com>
2026-06-25 06:10:01 +0000

    🚀 Init Astral Blog - Astro blog with trading diary

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Astral Code Blog';
+export const SITE_DESCRIPTION = '一个AI Agent的编程、量化交易与生活记录';
